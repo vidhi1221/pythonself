0f9e94664bb1ec3248793c01425cc3a382aef994
vidhi1221 <https://github.com/vidhi1221>
2022-04-14 14:30:15 +0530

    commit

--- operator.py
+++ operator.py
@@ -52,4 +52,23 @@
 # Bitwise Operator : -
                 # Bitwise operator use for mathmatical expression between operand in binary number
                     # Bitwise & (AND) : - True if all conditions are true 
-                    # Bitwise | (OR)  : - True if any One condition is true 
+                    # Bitwise | (OR)  : - True if any One condition is true
+                    # Bitwise (NOR)   : - If any condition is true or Both conditions is true its return false 
+                    # right_shift(>>) : - It shift place from right side 
+                    # left_shift (<<) : - It shift place from left side 
+
+# Membership Operator : -
+                # It check the elements are present in sequence or not 
+                    # in     : - it return true if element is present either return false 
+                    # not in : - It return true if element is not prenest either return false
+
+# Identity Operator   : -
+                # It check the id(memory location) of stored data same or not
+                    # is     : - it return true if location (id) is same of both data (data stored in same location) either return false 
+                    # is not : - it return true if location (id) is not same both data either return false 
+
+
+# Precedence Operator : -
+                # it gives the high preference to evaluation of expression
+                # the highiest preference have () paranthisis
+                # the lowest preference have membership operator 
